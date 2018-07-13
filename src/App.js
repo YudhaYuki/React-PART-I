905937d7c36e314a3df1d87fecb05822db9319e7
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-13 16:37:41 +0200

    Flexible List: Now we can edit every name on the lists

--- src/App.js
+++ src/App.js
@@ -18,15 +18,23 @@ class App extends Component {
 
   }
 
-  nameChangeHandler = (event) => {
-    // console.log('Was click!');
-    this.setState({
-      persons: [
-        { name: 'Yudha', age: 27 },
-        { name: event.target.value, age: 66 },
-        { name: 'Lieve', age: 57 }
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // Const person = Object.assign({}, this.state.persons[personIndex]);
+    
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
   deletePersonHandler = (personIndex) => {
@@ -60,7 +68,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name} 
                 age={person.age} 
-                key={person.id} />
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
         </div>
       );
