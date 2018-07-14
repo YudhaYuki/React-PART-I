0050f61a9d6fcc403a5bbf3dc2c9489f6731bad3
YudhaYuki <YudhaYuki@hotmail.com>
2018-07-14 15:09:51 +0200

    Install RADIUM : To enable CSS such as hover and Media Queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 
@@ -56,7 +57,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -76,6 +81,10 @@ class App extends Component {
       );
 
       style.backgroundColor= 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -104,4 +113,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
